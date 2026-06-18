595f7a970e605894266f782ed2a8c45af0c9facb
Evan Damas <92992183+Zenitude71@users.noreply.github.com>
2026-06-18 13:58:49 +0200

    Finalisation

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.notes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.example.notes.ui.navigation.NotesApp
+import com.example.notes.ui.theme.NotesTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,10 +12,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            /*
             NotesTheme {
                 NotesApp()
-            }*/
+            }
         }
     }
 }
